2628aa29f9e4732c4963a36cb5f621e1b1d62b56
Yunus Emre Sezgin <77413449+YunusEmre0909@users.noreply.github.com>
2022-09-10 14:05:06 +0300

    Initial commit

---
+++ Application/Features/ProgrammingLanguages/Queries/GetListProgrammingLanguage/GetListProgrammingLanguageQuery.cs
@@ -0,0 +1,43 @@
+﻿using Application.Features.ProgrammingLanguages.Models;
+using Application.Services;
+using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Queries.GetListProgrammingLanguage
+{
+    public class GetListProgrammingLanguageQuery : IRequest<ProgrammingLanguageListModel>
+    {
+        public PageRequest PageRequest { get; set; }
+
+        public class GetListProgrammingLanguageQueryHandler : IRequestHandler<GetListProgrammingLanguageQuery, ProgrammingLanguageListModel>
+        {
+            private readonly IMapper _mappper;
+            private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+
+            public GetListProgrammingLanguageQueryHandler(IMapper mappper, IProgrammingLanguageRepository programmingLanguageRepository)
+            {
+                _mappper = mappper;
+                _programmingLanguageRepository = programmingLanguageRepository;
+            }
+
+            public async Task<ProgrammingLanguageListModel> Handle(GetListProgrammingLanguageQuery request, CancellationToken cancellationToken)
+            {
+                IPaginate<ProgrammingLanguage> programmingLanguages = await _programmingLanguageRepository.GetListAsync
+                                                    (index: request.PageRequest.Page,size:request.PageRequest.PageSize);
+
+                ProgrammingLanguageListModel mappedProgrammingLanguageListModel = _mappper.Map
+                                                    <ProgrammingLanguageListModel>(programmingLanguages);
+
+                return mappedProgrammingLanguageListModel;
+            }
+        }
+    }
+}
---
+++ WebAPI/Controllers/ProgrammingLanguagesController.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.ProgrammingLanguages.Commands.CreateProgramminglanguage;
+using Application.Features.ProgrammingLanguages.Dtos;
+using Application.Features.ProgrammingLanguages.Models;
+using Application.Features.ProgrammingLanguages.Queries.GetListProgrammingLanguage;
+using Core.Application.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgrammingLanguagesController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguageCommand)
+        {
+            CreatedProgrammingLanguageDto result = await Mediator.Send(createProgrammingLanguageCommand);
+            return Created("",result);
+            
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListProgrammingLanguageQuery getListProgrammingLanguageQuery = new GetListProgrammingLanguageQuery { PageRequest =pageRequest};
+            ProgrammingLanguageListModel result = await Mediator.Send(getListProgrammingLanguageQuery);
+            return Ok(result);
+        }
+    }
+}
